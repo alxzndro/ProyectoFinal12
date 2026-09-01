c1dc4a885945075f565b9ef36b7588bba247fc59
ESTUDIANTES <ESTUDIANTES@puce.edu.ec>
2026-09-01 11:11:30 -0500

    cambio final

--- src/js/grafico.js
+++ src/js/grafico.js
@@ -1,9 +1,12 @@
+const reservas = [18, 25, 21, 32, 28, 40, 35];
+
 const options = {
+
   series: [
     {
       name: "Reservas",
       color: "#d4af37",
-      data: [18, 25, 21, 32, 28, 40, 35]
+      data: reservas
     }
   ],
 
@@ -69,3 +72,60 @@ if (document.querySelector("#column-chart")) {
 
   chart.render();
 }
+
+const precios = [
+  6.50, 3.00, 4.20, 4.50,
+  8.20, 4.20, 8.20, 5.40,
+  3.50, 2.00, 3.00, 4.50,
+  2.00, 2.20, 3.50, 3.50
+];
+
+// Calcular total de reservas
+const totalReservas = reservas.reduce(
+  (total, cantidad) => total + cantidad,
+  0
+);
+
+const sumaPrecios = precios.reduce(
+  (total, precio) => total + precio,
+  0
+);
+
+const precioPromedio = sumaPrecios / precios.length;
+
+const ingresoEstimado = totalReservas * precioPromedio;
+
+const metaSemanal = 750;
+
+const metaSuperada = ingresoEstimado > metaSemanal;
+
+const elementoReservas = document.querySelector("#total-reservas");
+const elementoPrecio = document.querySelector("#precio-promedio");
+const elementoIngreso = document.querySelector("#ingreso-estimado");
+const elementoResultado = document.querySelector("#resultado-meta");
+
+if (elementoReservas) {
+  elementoReservas.textContent = totalReservas;
+}
+
+if (elementoPrecio) {
+  elementoPrecio.textContent = `$${precioPromedio.toFixed(2)}`;
+}
+
+if (elementoIngreso) {
+  elementoIngreso.textContent = `$${ingresoEstimado.toFixed(2)}`;
+}
+
+if (elementoResultado) {
+  if (metaSuperada) {
+    elementoResultado.textContent =
+      "Meta Superada: Se obtuvieron los resultados esperados.";
+    elementoResultado
+  } else {
+    elementoResultado.textContent =
+      "No se alcanzo la meta semanal";
+    elementoResultado
+  }
+}
+
+
